54f5228c19aed8bcf9495972ec1f63cb948957a2
allan fortes <allaf333@gmail.com>
2022-02-13 21:15:58 -0300

    login Trrue

---
+++ js/script.js
@@ -0,0 +1,12 @@
+function login () {
+    var valipasso = window.document.getElementById("inpassoword").value;
+    var valemail = window.document.getElementById("inemail").value;
+    var es = window.document.getElementById("escre");
+    if (valipasso == "Allan" && valemail == "allan@gmail.com") {
+        es.innerHTML= "Ola allan"
+    } else {
+        es.innerHTML = `Senha Incorreta`
+    }
+
+}
+
